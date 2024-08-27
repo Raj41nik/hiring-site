06810a97aff6600fe7f5aba98700ee8ad5ce4ae8
Raj41nik <raj40rockstanik@gmail.com>
2024-08-27 22:18:06 +0530

    done vercel

--- jobforme/jobforme/settings.py
+++ jobforme/jobforme/settings.py
@@ -76,12 +76,12 @@ WSGI_APPLICATION = 'jobforme.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
 
 
 # Password validation
@@ -114,14 +114,11 @@ USE_I18N = True
 
 USE_TZ = True
 
-import os
+
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
-MEDIA_URLS ='/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 
 # Default primary key field type
@@ -129,10 +126,14 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-
+import os
 
 MEDIA_URL='/media/'
 
 MEDIA_ROOT=os.path.join(BASE_DIR,'media')
 
 LOGIN_URL='login_user'
+
+STATICFILES_DIRS= os.path.join(BASE_DIR,'static'),
+
+STATIC_ROOT=os.path.join(BASE_DIR,'staticfiles_build','static')
